6f6bfd2729387ea14c6da5d6af7ddfda0d23a30e
Bernard Sufrin <Bernard.Sufrin@cs.ox.ac.uk>
2022-06-30 19:58:40 +0100

    Create Glue.scala
    To add horizontal and vertical glue components.
    They are overweight at present.

---
+++ src/Red/Glue.scala
@@ -0,0 +1,25 @@
+package Red
+
+/**
+ *  Glue provision: this uses non-lightweight `BoxPanel`s in its implementation
+ *  as a matter of convenience. In principle
+ */
+
+import scala.swing.{BoxPanel, Component, Dimension, Orientation}
+
+class HGlue extends BoxPanel(Orientation.Horizontal) {
+  preferredSize = new Dimension(0, 0)
+  minimumSize   = new Dimension(0, 0)
+  maximumSize   = new Dimension(Int.MaxValue, 0)
+}
+
+class VGlue extends BoxPanel(Orientation.Vertical) {
+  preferredSize = new Dimension(0, 0)
+  minimumSize   = new Dimension(0, 0)
+  maximumSize   = new Dimension(0, Int.MaxValue)
+}
+
+object Glue {
+  def horizontal(): Component = new HGlue()
+  def vertical():   Component = new VGlue()
+}
